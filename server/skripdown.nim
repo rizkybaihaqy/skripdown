3aa46e71acec9db70a0a5314cc87407e741c11ad
hilmiwicak <hilmiwicaksonop@gmail.com>
2023-03-03 17:31:36 +0700

    fix server

--- server/skripdown.nim
+++ server/skripdown.nim
@@ -1,32 +1,27 @@
-import std/[tempfiles, os, osproc], jester
+import std/[tempfiles, os], jester
 
 routes:
   get "/":
-    const index = readFile "public/index.html"
+    const index = readFile "server/public/index.html"
     resp index
   post "/":
     let files: MultiData = request.formData
-    let dir = createTempDir("tmp_", "_", "uploads")
-    let skripsi = files.getOrDefault("skripsi").fields["filename"]
-    let bib = files.getOrDefault("bib").fields["filename"]
+    let dir = createTempDir("tmp_", "_", "server/uploads")
 
     for i, f in files.pairs:
       case i
       of "skripsi":
-        writeFile(dir & "/" & "srkipsi.md", f.body)
+        writeFile(dir & "/" & "skripsi.md", f.body)
       of "bib":
         writeFile(dir & "/" & "reference.bib", f.body)
       of "assets":
         writeFile(dir & "/" & f.fields["filename"], f.body)
       else: discard
 
-    let output = execProcess("pandoc", args = [dir & "skripsi.md", "--citeproc",
-        "--bibliography=" & dir & "reference.md", "--csl=../template/apa.csl",
-        "--pdf-engine=xelatex", "--metadata-file=../config.yaml",
-        "--template=../template/template.tex",
-        "--lua-filter=../lua-filters/list-table.lua",
-        "--lua-filter=../lua-filters/tables-rules.lua", "-o skripsi.pdf"],
-        options = {poUsePath})
+    let output = execShellCmd("pandoc " & dir &
+        "/skripsi.md --citeproc --bibliography=" & dir &
+        "/reference.bib --csl=template/apa.csl --pdf-engine=xelatex --metadata-file=config.yaml --template=template/template.tex --lua-filter=lua-filters/list-table.lua --lua-filter=lua-filters/tables-rules.lua --resource-path=" &
+        dir & " -o " & dir & "/skripsi.pdf")
 
     let pdf = readFile dir & "/skripsi.pdf"
 
